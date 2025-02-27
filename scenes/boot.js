ecb126a251464c0bb9a92d5d69ca2cac5cf99816
mispah interactives <mispahinteractives@gmail.com>
2025-02-27 20:39:49 +0530

    updated preloader

--- scenes/boot.js
+++ scenes/boot.js
@@ -9,19 +9,32 @@ export default class Boot extends Phaser.Scene {
         super({ key: 'boot' })
     }
 
-    init (){
-    }
+    init() {}
 
     preload() {
-        // Images
-        //this.load.image('Intro_Instuction', 'assets/Intro_Instuction.png');
-       
+        this.load.image('bg', 'assets/bg.png');
+        this.load.image('blue', 'assets/blue.png');
+        this.load.image('bg2', 'assets/bg2.png');
+        this.load.image('yellow', 'assets/yellow.png');
+        this.load.image('red', 'assets/red.png');
+        this.load.image('white', 'assets/white.png');
+        this.load.image('loading_bar_unfilled', 'assets/loading_bar_unfilled.png');
+        this.load.image('loading_bar_filled', 'assets/loading_bar_filled.png');
+        this.load.atlas('sheet', 'assets/sheet.png', 'assets/sheet.json');
+
+        this.load.script('webfont', 'lib/webfont.js');
+        this.load.plugin('rextagtextplugin', 'lib/rextagtextplugin.min.js', true);
+        // this.load.audio('bgm','sounds/bgm.mp3');
+        // this.load.audio('win','sounds/win.mp3');
+        this.load.audio('countdown', 'sounds/countdown.mp3');
+        this.load.audio('break', 'sounds/break.mp3');
+
     }
 
     create() {
 
-       
-       this.scene.stop('boot');
-       this.scene.launch('preload');
+
+        this.scene.stop('boot');
+        this.scene.launch('preload');
     }
 }
--- scenes/preload.js
+++ scenes/preload.js
@@ -1,12 +1,7 @@
 export default class Preload extends Phaser.Scene {
 
-    width = null
-    height = null
-    handlerScene = null
-    sceneStopped = false
-
     constructor() {
-        super({ key: 'preload' })
+        super({ key: 'preload' });
     }
 
     init() {
@@ -25,40 +20,62 @@ export default class Preload extends Phaser.Scene {
     }
 
     preload() {
-        // Images
-        this.load.image('bg', 'assets/bg.png');
-        this.load.image('blue', 'assets/blue.png');
-        this.load.image('bg2', 'assets/bg2.png');
-        this.load.image('yellow', 'assets/yellow.png');
-        this.load.image('red', 'assets/red.png');
-        this.load.image('white', 'assets/white.png');
-        this.load.atlas('sheet', 'assets/sheet.png', 'assets/sheet.json');
+        this.sceneStopped = false;
+        this.canvasWidth = this.sys.game.canvas.width;
+        this.canvasHeight = this.sys.game.canvas.height;
+        this.timeStart = Date.now();
 
-        this.load.script('webfont', 'lib/webfont.js');
-        this.load.plugin('rextagtextplugin', 'lib/rextagtextplugin.min.js', true);
-        // this.load.audio('bgm','sounds/bgm.mp3');
-        // this.load.audio('win','sounds/win.mp3');
-        this.load.audio('countdown', 'sounds/countdown.mp3');
-        this.load.audio('break', 'sounds/break.mp3');
+        this.bg = this.add.sprite(this.canvasWidth / 2, this.canvasHeight / 2, 'bg2');
+        this.bg.setOrigin(0.5).setScale(1);
 
-        //---------------------------------------------------------------------->
+        let scaleX = this.canvasWidth / this.bg.width;
+        let scaleY = this.canvasHeight / this.bg.height;
+        let scale = Math.max(scaleX, scaleY);
+        this.bg.setScale(scale);
 
-        this.canvasWidth = this.sys.game.canvas.width
-        this.canvasHeight = this.sys.game.canvas.height
+        this.bar = this.add.sprite(this.canvasWidth / 2, this.canvasHeight / 2, 'loading_bar_unfilled');
+        this.bar.setOrigin(0.5).setScale(.7);
 
-        this.width = this.game.screenBaseSize.width
-        this.height = this.game.screenBaseSize.height
+        this.loadingText = this.add.text(this.canvasWidth / 2, this.canvasHeight / 2 - 100, "Loading...", {
+            fontFamily: "Flame_Regular",
+            fontSize: "32px",
+            color: "#ffffff"
+        });
+        this.loadingText.setOrigin(0.5);
 
-        this.sceneStopped = false
+        this.fill = this.add.sprite(this.canvasWidth / 2, this.canvasHeight / 2, 'loading_bar_filled');
+        this.fill.setOrigin(0.5).setScale(.7, .65);
+        this.fill.orgWidth = this.fill.width;
 
+        this.cropRect = new Phaser.Geom.Rectangle(0, 0, 0, this.fill.height);
+        this.fill.setCrop(this.cropRect);
 
         this.load.on('progress', (value) => {
-
-        })
+            console.log("mm");
+            if (this.fill && this.fill.orgWidth) {
+                let val = (value) * this.fill.orgWidth * 0.5;
+                this.cropRect.width = val;
+                this.fill.setCrop(this.cropRect);
+            }
+        });
 
         this.load.on('complete', () => {
+            let difference = Date.now() - this.timeStart;
+            difference = Math.max(0, 1000 - difference);
 
-        })
+            this.tweens.add({
+                targets: this.cropRect,
+                width: this.fill.orgWidth,
+                duration: difference,
+                ease: "Power0",
+                onUpdate: () => {
+                    this.fill.setCrop(this.cropRect);
+                },
+                onComplete: () => {
+                    this.fill.setCrop(this.cropRect);
+                }
+            });
+        });
     }
 
     create() {
@@ -66,13 +83,10 @@ export default class Preload extends Phaser.Scene {
         this.firstTime = false;
         let _this = this;
         WebFont.load({
-
             custom: {
                 families: ['Flame_Regular']
-
             },
             active: function() {
-
                 _this.scene.stop('preload');
                 _this.scene.launch('GameScene');
             }
